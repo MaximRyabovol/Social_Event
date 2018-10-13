d756d4d9194bb9404344a915414287e886a8adbf
Maxim Ryabovol <work.amur@gmail.com>
2018-10-13 21:45:33 +0900

    base_commit

---
+++ app/src/main/java/com/eventdigital/socialevent2/MainActivity.java
@@ -0,0 +1,6 @@
+package com.eventdigital.socialevent2;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/eventdigital/socialevent2/fragments/RegistartionFragment.java
@@ -0,0 +1,23 @@
+package com.eventdigital.socialevent2.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eventdigital.socialevent2.R;
+
+public class RegistartionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.registartion_screen, container, false);
+
+        return v;
+    }
+}
